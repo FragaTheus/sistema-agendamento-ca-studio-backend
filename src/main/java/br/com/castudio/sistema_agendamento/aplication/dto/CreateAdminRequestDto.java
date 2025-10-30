62c664e5d1237518ee9ca2557af03f629a0f46e5
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2025-10-29 22:26:51 -0300

    commitando dto com teste unitario bem sucedidos

---
+++ src/main/java/br/com/castudio/sistema_agendamento/aplication/dto/CreateAdminRequestDto.java
@@ -0,0 +1,40 @@
+package br.com.castudio.sistema_agendamento.aplication.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAdminRequestDto {
+
+    @NotBlank
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÿ\\s]+$",
+            message = "Nome invalido"
+    )
+    String name;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Email invalido"
+    )
+    String email;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,30}$",
+            message = "Senha invalida"
+    )
+    String password;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,30}$",
+            message = "Senha invalida"
+    )
+    String confirmPassword;
+
+}
--- src/main/java/br/com/castudio/sistema_agendamento/domain/vo/PasswordVO.java
+++ src/main/java/br/com/castudio/sistema_agendamento/domain/vo/PasswordVO.java
@@ -5,13 +5,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public record PasswordVO(String value) {
 
     private static BCryptPasswordEncoder ENCODER = new BCryptPasswordEncoder();
-    private static String REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$\n";
+
 
     public PasswordVO{
-        if (value == null || !REGEX.matches(value)){
-            throw new IllegalArgumentException("Senha invalida");
-        }
-            value = ENCODER.encode(value);
+        value = ENCODER.encode(value);
     }
 
     public boolean isEqual(String password){
